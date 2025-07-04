3635436031b8ca51a693e7932ee349b0db1b0839
bruni3ra <igor.bruniera@escola.pr.gov.br>
2025-07-04 11:11:16 -0300

    1

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function inserirNome(){
     let nomeUsuario = prompt("Informe seu nome?");
-    let elemento = document.querySelector("#nome=usuario");
+    let elemento = document.querySelector("#nome-usuario");
     elemento.textContent = nomeUsuario;
 }
 
